4b53e2622987148c4836980ced8b2227cb77b818
Yaritza Bustamante <yaritzab99@gmail.com>
2024-11-07 11:04:57 -0500

    mountain dropdown

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,7 +1,45 @@
 "use strict";
 
-function loadMountains(params) {
+const mountainsDropdown = document.querySelector("#mountainsDropdown");
+const detailsButton = document.querySelector("#detailsButton")
+const mountainDetails = document.querySelector("#mountainDetails");
+const mountainImage = document.querySelector("#mountainImage");
+const cardTitleHeading = document.querySelector("#cardTitleHeading");
+const abbreviationParagraph = document.querySelector("#abbreviationParagraph");
+const mountainDetailsDiv =document.querySelector("#mountainDetailsDiv")
+
+
+
+function loadMountains() {
   for (const mountain of mountainsArray) {
-    console.log(mountain);
+    let option = document.createElement("option");
+    option.textContent = mountain.name;
+    option.value = mountain.name
+    mountainsDropdown.appendChild(option)
   }
 }
+
+loadMountains();
+
+function showMountainDetails() {
+  const mountainName = mountainsDropdown.value;
+  if (mountainName) {
+    mountainDetailsDiv.style.display = "block";
+  } else {
+    mountainDetailsDiv.style.display ="none";
+  }
+
+  for(const mountain of mountainsArray) {
+    if (mountain.name === mountainName){
+      mountainImage.src = `./images/${mountain.img}`;
+      cardTitleHeading.innerText = mountain.name;
+      abbreviationParagraph.innerText = mountain.desc;
+
+    }
+  }
+
+}
+
+
+
+
